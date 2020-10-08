a11cce894be53391dc979c1b5a04a2a5957bd63c
tprinz <prinz@splitbit.de>
2020-10-08 11:36:53 +0200

    Scaffold basic components

---
+++ components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>App Header</header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/language-button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button><slot /></button>
+</template>
+
+<script>
+export default {
+  name: 'LanguageButton'
+}
+</script>
+
+<style scoped>
+button {
+  color: #fff;
+  background: #106b39;
+  border-radius: 4px;
+  min-width: 320px;
+  height: 64px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 4px;
+}
+</style>
