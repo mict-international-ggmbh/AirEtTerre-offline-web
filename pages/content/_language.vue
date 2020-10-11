74fb5e9fc187478dc10b0e4be713fb5d0c25ff15
tprinz <prinz@splitbit.de>
2020-10-11 12:13:33 +0200

    Add content page

---
+++ pages/content/_language.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Say hello to:</div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+
+  validate({ params }) {
+    let valid = false
+    const languages = ['a', 'b', 'c']
+    valid = languages.includes(params.language)
+    return valid
+  }
+}
+</script>
+
+<style></style>
