1cd4f82a9a08d865e2a61ad0fc45c0926b6cbe36
tprinz <prinz@splitbit.de>
2020-10-17 16:31:02 +0200

    Refactor audio transcription to component

---
+++ components/audio-transcription-player.vue
@@ -0,0 +1,41 @@
+<template>
+  <vue-plyr ref="plyr" :options="options">
+    <audio />
+  </vue-plyr>
+</template>
+
+<script>
+export default {
+  name: 'AudioTranscriptionPlayer',
+
+  data: () => ({
+    player: undefined,
+    options: {
+      controls: [],
+      loadSprite: false,
+      iconUrl: '/plyr.svg'
+    }
+  }),
+
+  mounted() {
+    this.$nextTick(function () {
+      this.player = this.$refs.plyr.player
+    })
+  },
+
+  methods: {
+    play(src) {
+      this.player.source = {
+        type: 'audio',
+        sources: [
+          {
+            src,
+            type: 'audio/mp3'
+          }
+        ]
+      }
+      this.player.play()
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,11 @@
       :key="language.slug"
       :to="`${language.code}`"
       class="lang"
-      @play="play(`${translations[language.code].welcomeAudio}`)"
+      @play="$refs.player.play(translations[language.code].welcomeAudio)"
     >
       {{ translations[language.code].welcome }}
     </language-button>
-    <vue-plyr
-      ref="plyr"
-      :options="options"
-      :emit="['playing', 'pause', 'ended']"
-    >
-      <audio>
-        <source :src="playerSrc" type="audio/mp3" />
-      </audio>
-    </vue-plyr>
+    <audio-transcription-player ref="player" />
     <header>
       <h1>Air et terre</h1>
     </header>
@@ -34,40 +26,8 @@ export default {
   data: () => ({
     languages,
     translations,
-    player: undefined,
-    playerSrc: undefined,
-    options: {
-      controls: [],
-      loadSprite: false,
-      iconUrl: '/plyr.svg'
-    }
-  }),
-
-  mounted() {
-    this.$nextTick(function () {
-      this.player = this.$refs.plyr.player
-    })
-  },
-
-  methods: {
-    play(src) {
-      console.log('play', src)
-      console.log(this.player)
-      this.playerSrc = src
-      this.player.source = {
-        type: 'audio',
-        title: 'Example title',
-        sources: [
-          {
-            src,
-            type: 'audio/mp3'
-          }
-        ]
-      }
-
-      this.player.play()
-    }
-  }
+    audioTranscription: undefined
+  })
 }
 </script>
 <style scoped>
