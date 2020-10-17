712656cb8e36af97902a4412a11b6b745c06796b
tprinz <prinz@splitbit.de>
2020-10-17 18:20:51 +0200

    Apply header layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 /* eslint-disable prettier/prettier */
 <template>
   <div class="wrapper">
-    <language-button
-      v-for="language in languages"
-      :key="language.slug"
-      :to="`${language.code}`"
-      class="lang"
-      @play="$refs.player.play(i18n[language.code].audio.welcome)"
-    >
-      {{ i18n[language.code].welcome }}
-    </language-button>
-    <audio-transcription-player ref="player" />
+    <div class="languages">
+      <language-button
+        v-for="language in languages"
+        :key="language.slug"
+        :to="`${language.code}`"
+        class="lang"
+        @play="$refs.player.play(i18n[language.code].audio.welcome)"
+      >
+        {{ i18n[language.code].welcome }}
+      </language-button>
+      <audio-transcription-player ref="player" />
+    </div>
     <header>
       <h1>Air et terre</h1>
     </header>
@@ -32,12 +34,33 @@ export default {
 </script>
 <style scoped>
 .wrapper {
-  margin-top: 52px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.languages {
+  padding-top: 52px;
+}
+
+header {
+  min-height: 238px;
+  margin-left: -20px;
+  margin-right: -20px;
+  background-image: url('~assets/startpage-bg-1.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 h1 {
+  margin-top: 50px;
   font-weight: 500;
-  font-size: 24px;
+  font-size: 44px;
   line-height: 31px;
   /* or 129% */
   text-align: center;
